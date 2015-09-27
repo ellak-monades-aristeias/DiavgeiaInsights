d73b83a0f37716c5183d49efd7400dd7accdab45
Apostolos Vogklis <tvoglis@gmail.com>
2015-09-27 18:42:34 +0300

    Deliverable 1 : DB - Use Cases
    ODT documents about DB Schema design and main Requirement Analysis and
    Use Cases.

--- basic/config/web.php
+++ basic/config/web.php
@@ -3,7 +3,7 @@
 $params = require(__DIR__ . '/params.php');
 
 $config = [
-    'id' => 'basic',
+    'id' => 'diavgeiainsights',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
@@ -13,6 +13,9 @@ $config = [
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\PhpManager',
         ],
         'user' => [
             'identityClass' => 'app\models\User',
