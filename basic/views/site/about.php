fffed8cf6e22e175444838da6bf3d37f4fc2c933
Apostolos Vogklis <tvoglis@gmail.com>
2015-10-05 20:26:16 +0300

    About page
    About page text

--- basic/views/site/about.php
+++ basic/views/site/about.php
@@ -6,50 +6,21 @@ use yii\helpers\Html;
 //require_once('@vendor/opendata/opendata.php');
 require(__DIR__ . '/../../vendor/opendata/opendata.php');
 
-$this->title = 'About';
+$this->title = 'Σχετικά';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
-
-    <code><?= __FILE__ ?></code>
-    <br>
-<?php  
-// ΕΚΤΥΠΩΣΗ ΠΛΗΡΟΦΟΡΙΩΝ ΦΟΡΕΑ (UID, ΕΠΩΝΥΜΙΑ, ΜΟΝΑΔΕΣ, ΥΠΟΓΡΑΦΟΝΤΕΣ)
-$client = new OpendataClient();
-$client->setAuth('apiuser_1', 'ApiUser@1');
-$response = $client->getResource('/search?org=99206915&type=Β.2.1&size=100');
-echo $response->code;
-if ($response->code === 200) {    
-    $unitData = $response->data;
-	$sum = 0.0;
-	$counter = 1;
-    
-		foreach ($unitData['decisions'] as $unit) {
-			print $counter . ") ". $unit['subject'] . ": " . $unit['ada'] . " TYPE : " . $unit['decisionTypeId'] . ": ΗΜΕΡΟΜΗΝΙΑ : ". gmdate("Y-m-d", $unit['submissionTimestamp']/1000). " <a href=\"".  $unit['documentUrl'] . "\">Link</a> <br> \n";
-			$kaeData = $unit['extraFieldValues']['sponsor'];
-			//print_r($kaeData);
-			foreach ($kaeData as $kae) {
-				print  "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp ΠΡΟΜΗΘΕΥΤΗΣ : ".  $kae['sponsorAFMName']['afm'] . " - " . $kae['sponsorAFMName']['name'] . "<br> \n";
-				print  "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp ΚΑΕ : " . $kae['cpv'] . " ΠΟΣΟ : " . $kae['expenseAmount']['amount'] . "<br> \n";
-				$sum += $kae['expenseAmount']['amount'];
-			}	
-			$counter++;
-		}
-
-		print "<h2> ΣΥΝΟΛΙΚΟ ΠΟΣΟ : " . $sum . "€ </h2>";
- /*   
-    print "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Υπογράφοντες:<br>\n";
-    foreach ($orgData['signers'] as $signer) {
-        print "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;". $signer['uid'] . ": " . $signer['lastName'] . " " . $signer['firstName'] . "<br>\n";
-    }
-  */  
-} else {
-    echo "Error " . $response->code;
-}    
-
+    <div class="row">
+            <div class="col-lg-8">
+    <p>Το 'Πρόγραμμα Δι@υγεια' ή απλά Διαύγεια, δημιουργήθηκε το 2010 με το νόμο 3861/2010, έχοντας σαν στόχο την επίτευξη της μέγιστης δυνατής δημοσιότητας της κυβερνητικής πολιτικής και της διοικητικής δραστηριότητας, τη διασφάλιση της διαφάνειας και την εμπέδωση της υπευθυνότητας και της λογοδοσίας από την πλευρά των φορέων άσκησης της δημόσιας εξουσίας.   </p>
+    <p> Αποτελεί πλέον ένα πολύ σημαντικό κομμάτι του δημοσίου και δίνει στον πολίτη τη δυνατότητα να δει  αποφάσεις από διάφορους οργανισμούς και φορείς του δημοσίου, από ένα απλό οργανόγραμμα και τυπικές αποφάσεις μετακίνησης προσωπικού, έως και δαπάνες εκατομμυρίων για προμήθειες. Μέχρι την ημερομηνία σύνταξης του παρόντος υπάρχουν σύμφωνα με τη σελίδα της Διαύγειας [2], περίπου 15.300.000 αναρτήσεις αποφάσεων, σε διάστημα περίπου 5 ετών. Ο πραγματικά μεγάλος όγκων πληροφοριών μαζί με την οργάνωση της ιστοσελίδας, που βασίζεται μόνο στην εμφάνιση των δεδομένων, μας δυσκολεύουν στην αξιοποίηση και την ανάλυση τους.</p>
+    <p>Τον Ιούνιο του 2014 παρουσιάστηκε η δεύτερη έκδοση, η Διαύγεια ΙΙ, που παρόλο βελτιώνει το μηχανισμό αναζήτησης, δεν παρέχει στοιχεία ανάλυσης των δεδομένων, ή εκτεταμένα κριτήρια αναζήτησης.</p>
+    <p>Το κενό αυτό έχουν έρθει να καλύψουν και άλλα Project όπως η ΥπερΔι@υγεια [3] και το PublicSpending.net [4], με διαφορετικές προσεγγίσεις το καθένα, αλλά πάλι τα κριτήρια αναζήτησης αποφάσεων (κυρίως δαπανών) και η εμφάνιση στοιχείων είναι περιορισμένη.</p>
+             </div>
+        <div class="col-lg-4">
+            
+        </div>
+    </div>
 ?>
