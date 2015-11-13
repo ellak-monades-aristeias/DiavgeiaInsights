d9cc94723ffc0d257473a687e8cde158bc22ed07
Apostolos Vogklis <tvoglis@gmail.com>
2015-11-13 02:30:26 +0200

    Deliverables
    SQL scripts and manuals

--- PE1_DB/DB_Scripts/cost_per_afm.sql
+++ PE1_DB/DB_Scripts/cost_per_afm.sql
@@ -1,5 +1,5 @@
-SELECT awk.afm as ΑΦΜ, awk.name as ΕΠΩΝΥΜΙΑ, ROUND(SUM(awk.amount), 2) as ΠΟΣΟ, COUNT(awk.amount) as ΠΛΗΘΟΣ, ROUNTDAVG(awk.amount), 2) as ΜΟ
+SELECT awk.afm as ΑΦΜ, awk.name as ΕΠΩΝΥΜΙΑ, ROUND(SUM(awk.amount), 2) as ΠΟΣΟ, COUNT(awk.amount) as ΠΛΗΘΟΣ, ROUND(AVG(awk.amount), 2) as ΜΟ
 FROM decisions as dc, decisionsb21 as db21, amountwithkae as awk
 WHERE dc.ada=db21.b21_ada AND awk.awk_ada=dc.ada and dc.organizationId=99206915
 GROUP BY awk.afm
-ORDER BY AMNT DESC
+ORDER BY ΠΟΣΟ DESC
--- PE1_DB/DB_Scripts/sum_of_decisions_per_AFM_per_org.sql
+++ PE1_DB/DB_Scripts/sum_of_decisions_per_AFM_per_org.sql
@@ -1,4 +1,4 @@
-SELECT SUM(amk.amount), COUNT(amk.amount), dc.organizationId, org.label, amk.afm, amk.name
+SELECT SUM(amk.amount) as ΠΟΣΟ, COUNT(amk.amount) as ΠΛΗΘΟΣ , org.label as ΟΡΓΑΝΙΣΜΟΣ, amk.afm as ΑΦΜ, amk.name as ΟΝΟΜΑ
 FROM diavgeiainsights.decisions as dc, organisations as org, amountwithkae as amk
-WHERE org.uid=dc.organizationId and amk.awk_ada=dc.ada  and dc.decisionTypeId LIKE 'Β.2.1' 
-GROUP BY amk.afm
+WHERE org.uid=dc.organizationId and amk.awk_ada=dc.ada  and dc.decisionTypeId LIKE 'Β.2.1' and org.uid=6125
+GROUP BY amk.afmretailer_holidays
